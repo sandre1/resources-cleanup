898e25876df8b26a6d2a381d7dece2b0692bf052
Andrei <sandre1>
2023-06-08 12:54:01 +0300

    Added resource handler to check and copy files if exists?

--- src/resources_cleanup/core.clj
+++ src/resources_cleanup/core.clj
@@ -1,36 +1,32 @@
 (ns resources-cleanup.core
-  (:require [ring.adapter.jetty :as ring-jetty]
+  (:require [babashka.fs :as fs]
             [reitit.ring :as ring]
-            [muuntaja.core :as m]
-            [reitit.ring.middleware.muuntaja :as muuntaja])
+            [ring.adapter.jetty :as ring-jetty])
   (:gen-class))
 
+(defn check-and-move-resource! [src dest]
+  (if (fs/exists? src)
+    (fs/copy src dest)
+    (println "no files were found!!")))
 
-(defn string-handler [request]
-  (println (str request))
-  {:status 200
-   :body (str request)})
+(defn manage-resource [path]
+  (let [user-dir-path (System/getProperty "user.dir")
+        src-path (str user-dir-path path)
+        dest-path (str user-dir-path "/data/target")]
+    (println src-path)
+    (check-and-move-resource! src-path dest-path) ))
 
-(defn ping-handler [_]
-  {:status 200
-   :body "PING PING PING"})
-
-(defn pong-handler [_]
-  {:status 200
-   :body "PONG PONG PONG"})
+(defn resource-handler [req]
+  (let [path (:uri req)]
+    (println "calea este: " path)
+    (manage-resource path)
+    {:status 200
+     :body path}))
 
 (def app
   (ring/ring-handler
    (ring/router
-    ["/"
-     ["" string-handler]
-     ["ping" {:get ping-handler}]
-     ["pong" {:get pong-handler}]]
-    {:data {:muuntaja m/instance
-            :middleware [muuntaja/format-middleware]}})
-   (ring/routes
-    (ring/create-resource-handler {:path "/"})
-    (ring/create-default-handler))))
+    [["/*" resource-handler]])))
 
 (defn start [] (ring-jetty/run-jetty #'app {:port 3001
                                           :join? false}))
@@ -40,6 +36,10 @@
   (start))
 
 (comment 
-  (+ 1 1)
+  (System/getProperty "user.dir")
+  (if (fs/exists? "/home/nas/proiecte/resources-cleanup/data/source/some-doc.doc")
+    (fs/copy "/home/nas/proiecte/resources-cleanup/data/source/some-doc.doc" "/home/nas/proiecte/resources-cleanup/data/target/"))
+  (file-exists? "/")
   (-main)
+
   0)
