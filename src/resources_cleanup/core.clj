5ff3320cfbf452e1aa3d9db089f8886fa8299db1
Andrei <sandre1>
2023-06-08 11:34:38 +0300

    Added dummy files

--- src/resources_cleanup/core.clj
+++ src/resources_cleanup/core.clj
@@ -5,11 +5,17 @@
             [reitit.ring.middleware.muuntaja :as muuntaja])
   (:gen-class))
 
-(defn string-handler [_]
+
+(defn string-handler [request]
+  (println (str request))
   {:status 200
-   :body "On the project"})
+   :body (str request)})
 
 (defn ping-handler [_]
+  {:status 200
+   :body "PING PING PING"})
+
+(defn pong-handler [_]
   {:status 200
    :body "PONG PONG PONG"})
 
@@ -18,14 +24,22 @@
    (ring/router
     ["/"
      ["" string-handler]
-     ["ping" {:get ping-handler}]]
+     ["ping" {:get ping-handler}]
+     ["pong" {:get pong-handler}]]
     {:data {:muuntaja m/instance
-            :middleware [muuntaja/format-middleware]}})))
+            :middleware [muuntaja/format-middleware]}})
+   (ring/routes
+    (ring/create-resource-handler {:path "/"})
+    (ring/create-default-handler))))
 
-(defn start [] (ring-jetty/run-jetty app {:port 3000
+(defn start [] (ring-jetty/run-jetty #'app {:port 3001
                                           :join? false}))
 
 (defn -main
   [& args]
   (start))
 
+(comment 
+  (+ 1 1)
+  (-main)
+  0)
